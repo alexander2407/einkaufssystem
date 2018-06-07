6fb4a56afca98f89fc102a5bf23f7de8538c6918
dinho <dinho@10.201.55.13>
2018-06-08 01:06:46 +0200

    commit test1

--- inc/lieferantenbestellungen.inc.php
+++ inc/lieferantenbestellungen.inc.php
@@ -259,7 +259,7 @@ if (!isset($_GET['detail']) && !isset($_GET['aendern']) && !isset($_GET['neueBes
             }
             
             //echo "anzahl artikel: " . $anzahlArtikel;
-            $db->testTabelle($anzahlArtikel); //fügt in "testtab" die anzahl der artikelIDs des ausgewählten lieferanten ein.
+            $db->testTabelle($_POST['lieferant']); //fügt in "testtab" die anzahl der artikelIDs des ausgewählten lieferanten ein.
     }
     
 //        if(isset($_GET['bestellungAnlegen'])){
