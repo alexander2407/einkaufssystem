6fd9525309a46f1d98b66d26e95d3d0a5c0115e0
AlexB <AlexB@DESKTOP-HBF9T8D.mshome.net>
2018-06-11 09:48:08 +0200

    2. Version

--- inc/lieferanten.inc.php
+++ inc/lieferanten.inc.php
@@ -8,6 +8,14 @@ if (isset($_POST['submit_anlegen']) && isset($_POST['lieferantenname']) && isset
     $kontaktperson = new LieferantenKontaktperson(null, $_POST['vorname'], $_POST['nachname'], $_POST['telefonnummer'], $newId);
     $db->insertKontaktperson($kontaktperson);
     
+    $artikel = $db->getArtikel();
+    foreach ($artikel as $value) {
+        if(isset($_POST[$value->getArtikelId()])){
+            $lieferantliefert = new LieferantLiefert($newId, $value->getArtikelId(), null, null);
+            $db->insertLieferantLiefert($lieferantliefert);
+        }
+    }
+    
     if ($_POST['hausnummer'] <= 0) {
         echo "<div class='alert alert-danger' role='alert'> Hausnummer darf nicht kleiner gleich 0 sein. </div>";
         $_SESSION['errno'] = 1;
@@ -326,7 +334,7 @@ if (!isset($_GET['detail']) && !isset($_GET['aendern']) && !isset($_GET['neuerLi
         </div>
     </form>
     <?php
-}*/ else if (isset($_GET['neuerLieferant']) || $_SESSION['errno'] != 0 || isset($_GET['new2'])) {
+}*/ else if (isset($_GET['neuerLieferant']) || $_SESSION['errno'] != 0) {
     include './inc/lieferantAnlegen.inc.php';
 } else if (isset($_GET['aendern']) || $_SESSION['errno'] != 0) {
     include './inc/lieferantAendern.inc.php';
