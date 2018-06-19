7913cab29b4954e07983a963ea173aee991e22c8
Thomas <hillerthomas8@gmail.com>
2018-06-19 09:54:26 +0200

    Merge origin/master
    Conflicts:
    	index.php

--- inc/artikel.inc.php
+++ inc/artikel.inc.php
@@ -183,7 +183,7 @@ if (!isset($_GET['detail']) && !isset($_GET['aendern']) && !isset($_GET['neuerAr
         <div class = "form-group">
             <label for = "einkaufspreis" class = "col-sm-2 control-label">Einkaufspreis</label>
             <div class = "col-sm-10">
-                <input type = "text" value = "<?php echo $artikel->getEinkaufspreis();?>" name="einkaufspreis" class="form-control" id="einkaufspreis" required="">
+                <input type = "text" value = "<?php echo $artikel->getEinkaufspreis();?>" name="einkaufspreis" class="form-control" id="einkaufspreis" required="" pattern="^[1-9][0-9]?$|^100$" title="Bitte wählen Sie einen gültigen Betrag.">
             </div>
         </div>
 
